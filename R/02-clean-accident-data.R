702c8340e8dfcfc1ac7979c09d1f4973a830ae4c
Keisuke ANDO <ando@maslab.aitech.ac.jp>
2025-02-25 08:47:54 +0900

    :memo: Update file paths and add input/output documentation

--- R/02-clean-accident-data.R
+++ R/02-clean-accident-data.R
@@ -1,11 +1,19 @@
 # This script cleans the accident data and saves it as an RDS file.
+#
+# INPUTS:
+#   {INTERMEDIATE_DATA_DIR}/accident_data_path.rds: File paths to the downloaded
+#   traffic accident data files
+#
+# OUTPUTS:
+#   {INTERMEDIATE_DATA_DIR}/accident_data.rds: Cleaned accident data
+#
 
 # Get the directory for intermediate data from the environment variable
 intermediate_dir <- Sys.getenv("INTERMEDIATE_DATA_DIR")
 
 # Read the accident data path
 accident_data_path <- readRDS(
-  file.path(intermediate_dir, "01-accident_data_path.rds")
+  file.path(intermediate_dir, "accident_data_path.rds")
 )
 
 # Load accident data using the jpaccidents package function
@@ -14,5 +22,5 @@ accident_data <- jpaccidents::read_accident_data(accident_data_path)
 # Save the processed accident data for further use
 saveRDS(
   accident_data,
-  file = file.path(intermediate_dir, "02-accident_data.rds")
+  file = file.path(intermediate_dir, "accident_data.rds")
 )
